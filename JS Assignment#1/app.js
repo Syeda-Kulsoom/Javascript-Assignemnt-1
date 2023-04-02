674fba9ee2dfe11755b85bd9f388b69362654a34
Syeda-Kulsoom <skulsoom762@gmail.com>
2023-04-02 11:30:29 +0500

    1st assignment

---
+++ JS Assignment#1/app.js
@@ -0,0 +1,110 @@
+// chapter 1 (alert)
+// alert("Thanks for your input");
+
+// chapter 2 (variales)
+// var thanx = "Thanks for your input!"
+// alert(thanx)
+var name = 'Mark';
+name = 'ace';
+console.log(name);
+
+var nationality;
+nationality = 'US';
+console.log(nationality);
+
+var codername = 'kulsoom';
+var girlWhoKeepsSayingHisName = 'kulsoom'; 
+var x = 'kulsoom';
+console.log(codername, girlWhoKeepsSayingHisName, x);
+// chapter 3
+var weight = 150;
+weight = weight + 25;
+console.log(weight)
+
+var originalNumber = 23;
+var newNumber = originalNumber + 7;
+console.log(newNumber)
+
+var originalNumber = 23;
+var NumberTobeadded = 6;
+var newNumber = originalNumber + NumberTobeadded;
+console.log(newNumber)
+
+var originalNumber = 4;
+originalNumber = originalNumber + 4;
+console.log(originalNumber)
+
+var originalNumber = '23';
+var newNumber = originalNumber + 7;
+console.log(newNumber)
+
+// Chapter 5
+var popularNumber = 4;
+var popularNumber = 2 + 2;
+//alert(2 + 2);
+
+var popularNumber = 12 - 24;
+var popularNumber = 3 * 12;
+var num = 10;
+var popularNumber = num + 200;
+
+var num = 10;
+var anotherNumber = 200;
+var popularNumber = anotherNumber + num;
+//modulator operator
+var WhatNumberLeft = 9 % 3;
+
+//Math Epressions 
+//Unfamiliar Operators
+num++
+num = num + 1;
+
+var num = 1;
+var newNumber = num++;
+var newNumber = ++num;
+console.log(newNumber);
+
+var num = num--;
+var newNumber = --num;
+
+//chapter 7
+var totaalCost = 1 + 3 * 4;
+var totaalCost = 1 + (3 + 5);
+var resultofComputaton = (2+4)*(4+2);
+
+//chapter 8
+var message = 'Thanks';
+var userName = 'kulsoom';
+var banger = '!';
+var customMess = message + userName + banger;
+alert(customMess)
+
+// chapter 9
+var spec = prompt('Your species?, "human')
+
+var question = 'Your species?';
+var defaultAnswer = 'Human';
+var spec = prompt(question, defaultAnswer);
+console.log(spec);
+
+var numberofCats = prompt('How many cats?');
+var tooManyCats = numberofCats + 1;
+console.log(tooManyCats);
+
+// chapter 10
+var x = prompt('Whre does the pop live?');
+if (x == 'Vatican'){
+    alert("correct");
+}
+
+var correctAnswer = 'Vatican';
+if (x === correctAnswer){
+    alert("Correct!");
+}
+
+var correctAnswer = 'Vatican';
+if (x === correctAnswer){
+    score++;
+    userIQ = 'genius';
+    alert("Correct!");
+}
